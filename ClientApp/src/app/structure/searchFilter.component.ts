597b3a75fb05d236867ebdb9787028d136b83cf6
Troy Ostermann <Troy_Ostermann@hotmail.com>
2020-05-18 19:09:18 -0500

    Search feature added.

--- ClientApp/src/app/structure/searchFilter.component.ts
+++ ClientApp/src/app/structure/searchFilter.component.ts
@@ -9,6 +9,12 @@ export class SearchtermFilterComponent {
 
     constructor(private repo: Repository) { }
 
+    getSearchTerm() {
+        var userInput: string
+    
+        userInput = (document.getElementById("searchTerm") as HTMLInputElement).value;
+        this.setSearchTerm(userInput);
+    }
     setSearchTerm(searchTerm: string) {
         this.repo.filter.search = searchTerm;
         this.repo.getPeople();
