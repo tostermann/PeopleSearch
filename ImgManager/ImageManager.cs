59911387cd3a2c34724074e984e1f5bcaa44bb22
Troy Ostermann <Troy_Ostermann@hotmail.com>
2020-05-20 17:58:16 -0500

    Adding a static class to preform image manipulations

---
+++ ImgManager/ImageManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ImgManager
+{
+    using System.Drawing;
+    using System.IO;
+
+    public static class ImageManager
+    {
+        public static byte[] ImageToByteArray(Image imageIn)
+        {
+            using (var ms = new MemoryStream())
+            {
+                imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Gif);
+
+                return ms.ToArray();
+            }
+        }
+
+        public static Image ByteArrayToImage(byte[] byteArrayIn)
+        {
+            using (var ms = new MemoryStream(byteArrayIn))
+            {
+                var returnImage = Image.FromStream(ms);
+
+                return returnImage;
+            }
+        }
+
+        public static Image ConvertPicToImage(string filename)
+        {
+             return Image.FromFile(filename);
+        }
+    }
+}
